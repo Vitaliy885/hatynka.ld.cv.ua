eb70a231c78229fd46a3cd229c4d7a2aae6185d0
vitalii <vitalik@gmail.com>
2018-12-19 10:29:06 +0200

    uploading all files

---
+++ js/common.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+	$(document).ready(function(){
+		$('.btn-up').mPageScroll2id();
+	});
+	
+	$('.btn').click(function(){
+		$('nav ul').slideToggle(500);
+	});
+});
+
+
